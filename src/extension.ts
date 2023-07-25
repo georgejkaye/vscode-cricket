e6c4392a562d984bc74f24f7b58d1be26cb91b83
George Kaye <georgejkaye@gmail.com>
2023-07-25 22:54:27 +0100

    [feat] Add notification popups for each delivery

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { getSummary } from './tasks';
+import { getMatchData, getSummary } from './tasks';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -11,14 +11,38 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-cricket" is now active!');
 
+	const currentMatchKey = "currentMatch";
+	const lastDeliveryKey = "lastDelivery";
+	context.globalState.setKeysForSync([currentMatchKey]);
+	context.globalState.setKeysForSync([currentMatchKey]);
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let followMatch = vscode.commands.registerCommand('vscode-cricket.followMatch', async () => {
 		let matches = await getSummary();
+		let option = await vscode.window.showQuickPick(matches);
+		if(option) {
+			context.globalState.update(currentMatchKey, option.id);
+		}
 	});
 
 	context.subscriptions.push(followMatch);
+
+	const updateSeconds = 5;
+
+	setInterval(async () => {
+		let currentMatch : string | undefined = context.globalState.get(currentMatchKey);
+		if(currentMatch) {
+			let data = await getMatchData(currentMatch);
+			let lastBall = data.balls[0];
+			let lastDelivery = context.globalState.get(lastDeliveryKey);
+			if(!lastDelivery || lastDelivery !== lastBall.deliveryNo){
+				context.globalState.update(lastDeliveryKey, lastBall.deliveryNo)
+				vscode.window.showInformationMessage(`(${lastBall.deliveryNo}) ${lastBall.players} (${lastBall.event}). ${data.batting} are ${data.runs}/${data.wickets}.`);
+			}
+		}
+	}, updateSeconds * 1000);
 }
 
 // This method is called when your extension is deactivated
--- src/tasks.ts
+++ src/tasks.ts
@@ -4,9 +4,62 @@ import { xml2js } from "xml-js"
 const summaryXML = "http://static.cricinfo.com/rss/livescores.xml";
 
 interface Match {
-    title: string
+    label: string
     id: number
 }
+
+interface Ball {
+    runs : number
+    indicator: string
+    dismissal: string
+    extras: string
+    deliveryNo: string
+    players: string
+    event: string
+}
+
+interface Data {
+    balls: Ball[]
+    batting: string
+    runs: number
+    wickets: number
+}
+
+export const getMatchData = async (id : string) => {
+    let url = `https://www.espncricinfo.com/matches/engine/match/${id}.json`;
+    let response = await axios.get(url);
+    let data = response.data;
+
+    let comms : any[] = data.comms
+    let recentOvers : any[] = data.live.recent_overs
+
+    let deliveries = recentOvers.reverse().map((over : any[], overNumber) => {
+        let overComms = comms[overNumber];
+        return over.reverse().map((ball, ballNumber) => {
+            let ballComms = overComms.ball[ballNumber];
+            return {
+                runs: ball.ball === "&bull;" || ball.ball === "W" ? 0 : ball.ball,
+                indicator: ball.ball,
+                dismissal: ball.delivery,
+                extras: ball.extras,
+                event: ballComms.event,
+                players: ballComms.players,
+                deliveryNo: ballComms.overs_actual
+            };
+        });
+    });
+    let innings = data.innings;
+    let currentInnings = innings[innings.length - 1];
+    let battingTeamId = currentInnings.battingTeamId
+    let battingTeamName = data.team[0].contentId === battingTeamId ? data.team[0].team_name : data.team[1].team_name
+    return <Data>{
+        balls: deliveries.flat(),
+        runs: currentInnings.runs,
+        wickets: currentInnings.wickets,
+        batting: battingTeamName
+    };
+};
+
 
 export const getSummary = async () => {
     let response = await axios.get(summaryXML);
@@ -19,7 +72,7 @@ export const getSummary = async () => {
         let linkElements = link.split("/");
         let matchElement = linkElements[linkElements.length - 1];
         let matchId = matchElement.substring(0, matchElement.length - 18);
-        return { title: matchTitle, id: matchId };
+        return { label: matchTitle, id: matchId };
     });
     return matches;
 };
